454ffa9c0be85d5dfc935d2cbe48a53e04427540
cranzil <244459605@qq.com>
2023-04-26 16:42:00 +0800

    fix: 修复`timestamp` used before defined的问题

--- ios/Classes/TGFlutterPagRender.mm
+++ ios/Classes/TGFlutterPagRender.mm
@@ -55,10 +55,10 @@ static int64_t GetCurrentTimeUS() {
     if(duration <= 0){
         duration = 1;
     }
+    int64_t timestamp = GetCurrentTimeUS();
     if(start == 0){
         start = timestamp;
     }
-    int64_t timestamp = GetCurrentTimeUS();
     auto count = (timestamp - start) / duration;
     double value = 0;
     if (_repeatCount >= 0 && count >= _repeatCount) {
