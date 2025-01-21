aa1c267497d6526b8bae242ed51add9c1eee2187
liampan <liampan@tencent.com>
2025-01-21 14:57:01 +0800

    fix:synchronized前资源释放

--- ios/Classes/TGFlutterPagRender.mm
+++ ios/Classes/TGFlutterPagRender.mm
@@ -103,7 +103,9 @@ static int64_t GetCurrentTimeUS() {
         if ([[TGFlutterWorkerExecutor sharedInstance] enableMultiThread]) {
             // 防止setup和release、dealloc并行争抢
             @synchronized(self) {
-                [self setUpPlayerWithPagData:pagData];
+                if(self){
+                    [self setUpPlayerWithPagData:pagData];
+                }
             }
         } else{
             [self setUpPlayerWithPagData:pagData];
@@ -198,8 +200,10 @@ static int64_t GetCurrentTimeUS() {
     if (_surface) {
         if ([[TGFlutterWorkerExecutor sharedInstance] enableMultiThread]) {
             @synchronized(self) {
-                [_surface freeCache];
-                [_surface clearAll];
+                if (_surface){
+                    [_surface freeCache];
+                    [_surface clearAll];
+                }
             }
         } else{
             [_surface freeCache];
@@ -212,7 +216,7 @@ static int64_t GetCurrentTimeUS() {
     _frameUpdateCallback = nil;
     _eventCallback = nil;
     _surface = nil;
-    self.pagFile = nil;
-    self.player = nil;
+    _pagFile = nil;
+    _player = nil;
 }
 @end
